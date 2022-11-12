929a068ebb608806cca8255d87dfaa5ec9cdf6b7
MuhammadIhsan45 <muhsanz41@gmail.com>
2022-11-12 15:50:12 +0700

    commite kedua

--- src/main/java/com/example/FormKTM/controllerKTM.java
+++ src/main/java/com/example/FormKTM/controllerKTM.java
@@ -10,6 +10,7 @@ import java.util.Date;
 import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -21,22 +22,21 @@ import org.springframework.web.multipart.MultipartFile;
  */
 @Controller
 public class controllerKTM {
-   @RequestMapping("/hallo")
-   @ResponseBody
+   @RequestMapping("/viewpage")
    public String getData(@RequestParam("nama") String nama,
-           @RequestParam("NIM") String nim,
-           @RequestParam("Prodi") String prodi,
-           @RequestParam("gambar") MultipartFile gambar
+           @RequestParam("nim") String nim,
+           @RequestParam("prodi") String prodi,
+           @RequestParam("gambar") MultipartFile gambar,
+           Model ktm
    ) throws IOException{
-       nama = textProcess(nama);
-       nim = textProcess(nim);
-       prodi = textProcess(prodi);
-       
-       String blob = Base64.encodeBase64String(gambar.getBytes());
-       return nama + nim + prodi + "<br><img src='data:image/jpeg;base64,"+blob+" ' />";
-   }
-   private String textProcess(String nama,){
-       String result ="";
-       return result;
+        String blob = Base64.encodeBase64String(gambar.getBytes());
+        String gambarasal= "data:image/jpg;base64,".concat(blob);
+        
+        ktm.addAttribute("nma", nama);
+        ktm.addAttribute("nim", nim);
+        ktm.addAttribute("prd", prodi);
+        ktm.addAttribute("gbr", gambarasal);
+        
+        return "viewpage";
    }
 }
